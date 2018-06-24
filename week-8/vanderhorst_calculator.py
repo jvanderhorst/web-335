d77f64e0e789d871b2f9e556642af87c2ac634c5
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2018-06-24 19:48:07 -0400

    Adding week 8 assignments

---
+++ week-8/vanderhorst_calculator.py
@@ -0,0 +1,18 @@
+# add function
+def add(a, b):
+    return a + b
+
+# subtract function
+def subtract(a, b):
+    return a - b
+
+# divide function
+def divide(a, b):
+    return a/b
+
+
+print(add(7,8))
+print(subtract(17,5))
+print(divide(18,3))
+
+
